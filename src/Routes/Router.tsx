131bf0d47341a1b30585823ed6dff89cbd37a11f
movie42 <movie3342@gmail.com>
2022-10-31 02:18:42 +0900

    기타 수정

--- src/Routes/Router.tsx
+++ src/Routes/Router.tsx
@@ -9,7 +9,7 @@ const Router = () => {
     <Routes>
       <Route path="/" element={<Layout />}>
         <Route index element={<Home />} />
-        <Route path="detail/:id" element={<Detail />} />
+        <Route path="detail/:issue_number" element={<Detail />} />
       </Route>
       <Route path="*" element={<NotFound />} />
     </Routes>
