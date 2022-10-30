df898ca5983e78e5aeea90745d457f8b525b4f23
rayoungpark <rayoung1229@gmail.com>
2022-10-31 00:09:42 +0900

    design: 반응형 UI 적용

--- src/lib/components/layouts/Layout/styles.tsx
+++ src/lib/components/layouts/Layout/styles.tsx
@@ -3,10 +3,11 @@ import styled from "styled-components";
 export const LayoutWrapper = styled.div`
   height: 100%;
   min-height: 100vh;
+  position: relative;
 `;
 export const LayoutContentWrapper = styled.div`
   max-width: 1280px;
   padding: 1rem;
-  min-height: calc(100vh - 182px);
+  min-height: calc(100vh - 150px);
   margin: 0 auto;
 `;
--- src/lib/components/layouts/MoveTopButton/index.tsx
+++ src/lib/components/layouts/MoveTopButton/index.tsx
@@ -1,23 +1,61 @@
 import { useCallback, useEffect, useState } from "react";
+import styled from "styled-components";
+import { BsArrowBarUp } from "react-icons/bs";
 
 const MoveTopButton = () => {
   const [show, setShow] = useState(false);
   const onClickTopButton = useCallback(() => {
-    window.scrollTo(0, 0);
+    // window.scrollTo(0, 0);
+    window.scroll({ behavior: "smooth", top: 0 });
   }, []);
 
   useEffect(() => {
-    if (window.scrollY > 100) {
-      if (!show) setShow(true);
-    } else if (show) setShow(false);
+    let timer: NodeJS.Timeout;
+    const onScroll = () => {
+      clearTimeout(timer);
+      timer = setTimeout(() => {
+        if (window.scrollY > 100) {
+          setShow(true);
+        } else setShow(false);
+      }, 100);
+    };
+    window.addEventListener("scroll", onScroll);
+
+    return () => {
+      window.removeEventListener("scroll", onScroll);
+    };
   }, []);
 
   if (!show) return <></>;
   return (
-    <div>
-      <button onClick={onClickTopButton}>위로가기</button>
-    </div>
+    <MoveToButtonWrapper>
+      <button onClick={onClickTopButton}>
+        <BsArrowBarUp />
+      </button>
+    </MoveToButtonWrapper>
   );
 };
 
 export default MoveTopButton;
+
+const MoveToButtonWrapper = styled.div`
+  position: fixed;
+  bottom: 30px;
+  right: 10%;
+
+  & button {
+    background: #b13138;
+    width: 40px;
+    height: 40px;
+    border: none;
+    font-size: 20px;
+    border-radius: 10px;
+  }
+
+  & svg {
+    color: white;
+    font-size: 26px;
+    text-align: center;
+    vertical-align: middle;
+  }
+`;
